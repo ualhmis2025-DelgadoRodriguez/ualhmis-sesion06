236cd72e12bf1f794b96ca717e67082913b1888c
ualpda629 <pda629@inlumine.ual.es>
2025-04-09 16:10:01 +0200

    Subida de archivos

--- sesion06/src/test/java/ual/hmis/sesion06/ejercicio1/Ejercicio1Test.java
+++ sesion06/src/test/java/ual/hmis/sesion06/ejercicio1/Ejercicio1Test.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
 import ual.hmis.sesion06.ejercicio1.Ejercicio1;
 
 
